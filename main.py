1c446fd6752031ab4d3e5dc7f0f4084615f0aaab
lex <nathan.richard.vg@gmail.com>
2026-06-05 19:42:11 +0200

    test

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
 import pyxel
-import os
+
+
+class enemy:
+    def __init__(self, speed, hp, effects, position_x, position_y):
+        
+        self.speed = speed
+        self.hp = hp
+        self.effects = effects
+
+        self.position_x = position_x
+        self.position_y = position_y
+
+    def draw(self, sprite):
+        self.sprite = sprite
+
+    def movement(self, x, y):
+        self.position
+
+
+
+
+
+
+
+
+def update():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit
+
+def draw():
+    pyxel.cls(0)
+    pyxel.rect(10, 10, 20, 20, 10)
+
+
+
 
 if __name__ == "main":
+    pyxel.init(128, 128, title="Nuit du code")
+
+    pyxel.run(update, draw)
     main()
